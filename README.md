35ac35f929cd182ded727eaa7028f7922f5be2b0
wangzongming <1746809408@qq.com>
2025-04-22 22:12:03 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,33 @@
+# esp-ai-plugin-aliyun-cosyvoice [![npm](https://img.shields.io/npm/v/esp-ai-plugin-aliyun-cosyvoice.svg)](https://www.npmjs.com/package/esp-ai-plugin-aliyun-cosyvoice) [![npm](https://img.shields.io/npm/dm/esp-ai-plugin-aliyun-cosyvoice.svg?style=flat)](https://www.npmjs.com/package/esp-ai-plugin-aliyun-cosyvoice)
+
+让 esp-ai 支持阿里云的 cosyvoice 语音克隆服务
+
+ 
+
+# 安装
+在你的 `ESP-AI` 项目中执行下面命令
+```bash
+npm i esp-ai-plugin-aliyun-cosyvoice
+```
+
+# 使用 
+```js
+const espAi = require("esp-ai"); 
+
+espAi({ 
+    // 配置使用插件并且为插件配置api-key
+    tts_server: "esp-ai-plugin-aliyun-cosyvoice",
+    tts_config: {
+        // 获取地址： https://bailian.console.aliyun.com/#/api-key
+        api_key:"xxx",
+        // 获取地址： https://bailian.console.aliyun.com/   用cosyvoice克隆接口克隆的音色去克隆拿到的id，然后填入。
+        voice_id:"xxx"
+    }, 
+
+    // 引入插件
+    plugins: [ 
+        require("esp-ai-plugin-aliyun-cosyvoice")
+    ], 
+});
+```
+
---
+++ index.js
@@ -0,0 +1,148 @@
+
+const WebSocket = require('ws');
+const uuid = require('uuid').v4;
+
+module.exports = {
+    // 插件名字
+    name: "esp-ai-plugin-aliyun-cosyvoice",
+    // 插件类型 LLM | TTS | IAT
+    type: "TTS",
+    async main({ text, tts_config, cb, log, ttsServerErrorCb, connectServerCb, connectServerBeforeCb, logWSServer, session_id }) {
+        try {
+            const { api_key, voice_id } = tts_config; 
+
+            // WebSocket服务器地址
+            const url = 'wss://dashscope.aliyuncs.com/api-ws/v1/inference/';
+            const taskId = uuid();
+            let shouldClose = false;
+            let taskStarted = false;
+            let taskFinished = false;
+            // 发送run-task指令
+            const runTaskMessage = JSON.stringify({
+                header: {
+                    action: 'run-task',
+                    task_id: taskId,
+                    streaming: 'duplex'
+                },
+                payload: {
+                    task_group: 'audio',
+                    task: 'tts',
+                    function: 'SpeechSynthesizer',
+                    model: 'cosyvoice-v1',
+                    parameters: {
+                        text_type: 'PlainText',
+                        voice: voice_id, // 音色
+                        format: 'mp3', // 音频格式 
+                        sample_rate: 16000, // 采样率 
+                        volume: 90, // 音量
+                        rate: 1, // 语速
+                        pitch: 1 // 音调
+                    },
+                    input: {}
+                }
+            }, null, 4);
+
+            connectServerBeforeCb();
+            // 创建WebSocket客户端
+            const _ws = new WebSocket(url, {
+                headers: {
+                    Authorization: `bearer ${api_key}`,
+                    'X-DashScope-DataInspection': 'enable'
+                }
+            });
+            const ws = {
+                close() {
+                    shouldClose = true;
+                    sendEnd();
+                    _ws.close();
+                }
+            };
+            logWSServer(ws)
+
+
+            _ws.on('open', () => {
+                connectServerCb(true);
+                _ws.send(runTaskMessage);
+            });
+
+            _ws.on('message', (data, isBinary) => {
+                if (isBinary) { 
+                    !shouldClose && cb({ is_over: false, audio: data, ws });
+                } else {
+                    const message = JSON.parse(data);
+                    switch (message.header.event) {
+                        case 'task-started':
+                            taskStarted = true;
+                            // 发送continue-task指令
+                            sendContinueTasks(_ws);
+                            break;
+                        case 'task-finished':
+                            taskFinished = true;
+                            const finishedTodo = () => { 
+                                cb({ is_over: true, audio: "", ws });
+                                _ws.close();
+                            }
+                            // ESP-AI TTS 框架缺陷...
+                            if (session_id === "1001") {
+                                finishedTodo();
+                            } else {
+                                setTimeout(finishedTodo, 3000)
+                            } 
+                            break;
+                        case 'task-failed':
+                            ttsServerErrorCb(`TTS 任务失败：${message.header.error_message}`)
+                            log.error(runTaskMessage)
+                            _ws.close();
+                            break;
+                        default:
+                            // 可以在这里处理result-generated
+                            break;
+                    }
+                }
+            });
+
+            function sendEnd() {
+                if (taskFinished) return;
+                // 发送finish-task指令
+                const finishTaskMessage = JSON.stringify({
+                    header: {
+                        action: 'finish-task',
+                        task_id: taskId,
+                        streaming: 'duplex'
+                    },
+                    payload: {
+                        input: {}
+                    }
+                });
+                _ws.send(finishTaskMessage);
+            }
+
+            function sendContinueTasks() {
+                const continueTaskMessage = JSON.stringify({
+                    header: {
+                        action: 'continue-task',
+                        task_id: taskId,
+                        streaming: 'duplex'
+                    },
+                    payload: {
+                        input: {
+                            text
+                        }
+                    }
+                });
+                _ws.send(continueTaskMessage);
+                // 发送finish-task指令
+                sendEnd();
+            }
+
+            _ws.on('close', () => {
+                connectServerCb(false);
+            });
+
+        } catch (err) {
+            connectServerCb(false);
+            log.error(`阿里云 TTS 错误： ${err}`)
+        }
+
+    }
+}
